b98eedc68457f2aa28c5e5f59e9d10625414c679
Chaffexd <shanechaffe@gmail.com>
2023-09-13 17:17:44 +0100

    robot

--- next.config.js
+++ next.config.js
@@ -4,3 +4,21 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
+ };
