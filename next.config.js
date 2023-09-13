54eff19095fee70f03a30efe8cecdd8e41933c87
Chaffexd <shanechaffe@gmail.com>
2023-09-13 17:19:55 +0100

    again

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
 module.exports = {
   async headers() {
     const headers = [];
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
       headers.push({
         headers: [
           {
-            key: 'X-Robots-Tag',
-            value: 'noindex',
+            key: "X-Robots-Tag",
+            value: "noindex",
           },
         ],
-        source: '/:path*',
+        source: "/:path*",
       });
     }
     return headers;
   },
- };
+};
